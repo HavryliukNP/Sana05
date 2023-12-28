7c5f77fcaa9470555b55f19eece5ccec9678e1fa
Nazarii Havryliuk <ipz234_gnp@student.ztu.edu.ua>
2023-12-28 16:08:40 +0200

    Змінив Program.cs

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -6,11 +6,11 @@ MyDate myStartDate = new MyDate(2023, 12, 11, 16, 0);
 MyDate myFinishDate = new MyDate(2023, 12, 11, 18, 30);
 
 Airplane myAirplane = new Airplane("Місто1", "Місто2", myStartDate, myFinishDate);
-Console.WriteLine(myAirplane.GetTotalTime());
-Console.WriteLine(myAirplane.IsArrivingToday());
+Console.WriteLine($"Час польоту: {myAirplane.GetTotalTime()} хвилин");
+Console.WriteLine($"Виліт та посадка в один день: {myAirplane.IsArrivingToday()}");
 var currency = new Currency("USD", 37);
 var product = new Product("Телевізор", 100, currency, 5, "Samsung", 10);
 
-Console.WriteLine(product.GetPriceInUAH());
-Console.WriteLine(product.GetTotalPriceInUAH());
-Console.WriteLine(product.GetTotalWeight());
+Console.WriteLine($"Ціна товару в гривнях: {product.GetPriceInUAH()}");
+Console.WriteLine($"Загальна ціна товару в гривнях: {product.GetTotalPriceInUAH()}");
+Console.WriteLine($"Загальна вага усіх товарів: {product.GetTotalWeight()}");
